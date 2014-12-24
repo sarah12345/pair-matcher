9ec81fc8138af9419b3d9e8e97db107220908a04
Sarah Cnota <scnota@gmail.com>
2014-12-23 18:01:20 -0600

    move page from groups to users configure

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, path: "/", only: [] do
-    resources :groups, only: [:index]
+    get 'configure', on: :member
   end
 
   root 'welcome#index'
