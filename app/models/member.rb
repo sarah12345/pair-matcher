c043fce8a62e484a16081f140f37cdaac11b7807
Sarah Cnota <scnota@gmail.com>
2015-09-09 11:21:30 -0500

    order members by name

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,6 +3,7 @@ class Member < ActiveRecord::Base
   belongs_to :team
 
   validates_presence_of :first_name, :last_name
+  default_scope { order('first_name ASC, last_name ASC') }
 
   def name
     "#{first_name} #{last_name}"
